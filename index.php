5fac87d679b386de8c25608680a2f92369950b82
Hamid Ahmed Antor <60336275+hamidahmedantor@users.noreply.github.com>
2020-03-16 13:16:47 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title>X-COMPANY</title>
+	<style>
+		body{
+			margin:0;
+			padding:0px 50px;
+		}
+		a{
+			text-decoration:none;
+		}
+		.header_area{
+			width:100%;
+			
+			
+		}
+		.logoarea{
+			width:40%;
+			float:left;
+			background-color:#bae8e8;
+		}
+		.logoarea h1{
+			padding-left:30px;
+		}
+		.menu_area{
+			width:60%;
+			float:left;
+			background-color:#273746;
+		}
+		.menu_area ul{
+			list-style:none;
+			text-align:right;
+		}
+		.menu_area ul li{
+			display:inline-block;
+			padding:15px;
+			color:white;
+		}
+		.menu_area ul li a{
+			
+			color:white;
+		}
+		.content_area{
+			height:500px;
+			width:100%;
+			overflow:hidden;
+			background-color:#bae8e8;
+		}
+		
+		.footer_area{
+			width:100%;
+			overflow:hidden;
+			background-color:#bae8e8;
+			color:white
+		}
+		.footer_area h3{
+			text-align:center;
+		}
+	</style>
+</head>
+<body>
+	<div class="header_area">
+		<div class="logoarea">
+			<h1><span class="x"></span>Company</h1>
+		</div>
+		<div class="menu_area">
+			<ul>
+				<li><a href="index.php">Home</a></li>
+				<li><a href="log.php">Login</a></li>
+				<li><a href="regi.php">Registration</a></li>
+			</ul>
+		</div>
+	</div>
+	<div class="content_area">
+		<h1>Thanks for visiting !</h1>
+	</div>
+	<div class="footer_area">
+		<h3>&copy;2020</h3>
+	</div>
+</body>
+</html>
---
+++ odb.sql/odb.sql
@@ -0,0 +1,102 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 15, 2020 at 07:48 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.3
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `odb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `products`
+--
+
+CREATE TABLE `products` (
+  `id` int(100) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `description` varchar(100) NOT NULL,
+  `quantity` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`id`, `name`, `description`, `quantity`) VALUES
+(1, 'milk', 'good', '3');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(100) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `picture` varchar(300) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `username`, `password`, `picture`) VALUES
+(1, 'hamid', 'hamid@gmail.com', 'hamid', '12345', 'uploads/php.PNG');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
